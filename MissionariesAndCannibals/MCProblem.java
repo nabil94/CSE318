8ee63d46091f93d4f51c51fdd301bbebc4b96f2a
nabil94 <raianlatif1151041@gmail.com>
2019-07-15 20:56:55 +0600

    MC prob

---
+++ MissionariesAndCannibals/MCProblem.java
@@ -0,0 +1,151 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mcproblem;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.Vector;
+
+/**
+ *
+ * @author User
+ */
+public class MCProblem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int missionaries, cannibals, capacity;
+        Scanner m = new Scanner(System.in);
+        missionaries = m.nextInt();
+        Scanner c = new Scanner(System.in);
+        cannibals = c.nextInt();
+        Scanner b = new Scanner(System.in);
+        capacity = b.nextInt();
+        if ((cannibals > missionaries) || capacity == 0) {
+            System.out.println("cannot go to other bank");
+        } else {
+            State s = new State(missionaries, cannibals, 1, 0, 0, capacity);
+            System.out.println("BFS Solve");
+            long t1 = System.currentTimeMillis();
+            State n = BFSSolution(s);
+            if (n == null) {
+                System.out.println("Not Solvable");
+            } else {
+                n.printPath();
+            }
+            long t2 = System.currentTimeMillis();
+            //if((t2 - t1) < 30000){
+            System.out.println("time needed " + (t2 - t1) + " milliseconds");
+            //}
+            
+            System.out.println();
+            System.out.println("DFS Solve");
+            long t3 = System.currentTimeMillis();
+            //DFSSolution(s);
+            State nn = DFSSolution(s);
+            if (nn == null) {
+                System.out.println("Not Solvable");
+            } else {
+                nn.printPath();
+            }
+            long t4 = System.currentTimeMillis();
+            //if((t4 - t3) < 30000){
+            System.out.println("time needed " + (t4 - t3) + " milliseconds");
+            //}
+            
+        }
+
+    }
+
+    public static State BFSSolution(State source) {
+        Queue<State> q = new LinkedList<>();
+        Vector<State> visited = new Vector<>();
+        q.add(source);
+        //Pair pr = new Pair(source_State,"Initial State");
+        visited.addElement(source);
+        //System.out.println(q.peek().state.toString()+" "+q.peek().edge);
+        long t = System.currentTimeMillis() + 120000;
+        while (true) {
+            if (q.isEmpty() || q.size() > 99999) {
+                return null;
+                //break;
+            }
+            
+            if(System.currentTimeMillis() > t){
+                System.out.println("Time limit crossed");
+                return null;
+            }
+            State n = q.peek();
+            //System.out.println(n.state.toString());
+            q.remove();
+            Vector<State> vec = n.getChildStates();
+            for (int i = 0; i < vec.size(); i++) {
+                State p = vec.elementAt(i);
+                //String s = vec.elementAt(i).edge;
+                for (int j = 0; j < visited.size(); j++) {
+                    if (!visited.contains(p)) {
+                        State newState = new State(p.mlb,p.clb,p.boat,p.mrb,p.crb,p.boatCapacity,n);
+                        if (p.IsGoalNode()) {
+                            //System.out.println(q.size());
+                            return newState;
+                            //break;
+                        }
+                        q.add(newState);
+                        visited.addElement(p);
+
+                    }
+                }
+            }
+        }
+
+    }
+
+    public static State DFSSolution(State source) {
+        Stack<State> st = new Stack<>();
+        st.push(source);
+        Vector<State> visited = new Vector<>();
+        visited.addElement(source);
+        long t = System.currentTimeMillis() + 120000;
+        while (true) {
+            if (st.empty() || st.size() > 99999) {
+                return null;
+                //System.out.println("No solution");
+                //break;
+            }
+            if(System.currentTimeMillis() > t){
+                System.out.println("Time limit crossed");
+                return null;
+            }
+            State n = st.peek();
+            //System.out.println(n.state.toString());
+            st.pop();
+            Vector<State> vec = n.getChildStates();
+            for (int i = 0; i < vec.size(); i++) {
+                State p = vec.elementAt(i);
+                //String s = vec.elementAt(i).edge;
+                for (int j = 0; j < visited.size(); j++) {
+                    if (!visited.contains(p)) {
+                        State newState = new State(p.mlb,p.clb,p.boat,p.mrb,p.crb,p.boatCapacity,n);
+                        if (p.IsGoalNode()) {
+                           // System.out.println(st.size());
+                            return newState;
+                        }
+                        st.push(newState);
+                        visited.addElement(p);
+
+                    }
+                }
+            }
+        }
+
+    }
+    
+}
---
+++ MissionariesAndCannibals/State.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mcproblem;
+
+import java.util.Vector;
+
+/**
+ *
+ * @author User
+ */
+public class State {
+
+    int mlb, clb;
+    int mrb, crb;
+    int boat;
+    int boatCapacity;
+    State ParentState;
+    int path_cost;
+
+    public State(int mlb, int clb, int boat, int mrb, int crb, int boatCapacity) {
+        this.mlb = mlb;
+        this.mrb = mrb;
+        this.clb = clb;
+        this.crb = crb;
+        this.boat = boat;
+        this.boatCapacity = boatCapacity;
+        ParentState = null;
+        path_cost = 0;
+    }
+    
+    public State(int mlb, int clb, int boat, int mrb, int crb, int boatCapacity, State ParentState) {
+        this.mlb = mlb;
+        this.mrb = mrb;
+        this.clb = clb;
+        this.crb = crb;
+        this.boat = boat;
+        this.boatCapacity = boatCapacity;
+        this.ParentState = ParentState;
+        path_cost = ParentState.path_cost + 1;
+    }
+
+    public boolean IsGoalNode() {
+        if (mlb == 0 && clb == 0 && boat == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String toString() {
+        return "(" + mlb + " " + clb + " " + boat + " " + mrb + " " + crb + ")";
+    }
+
+    public boolean equals(Object obj) {
+        if (!(obj instanceof State)) {
+            return false;
+        }
+        State s = (State) obj;
+        return (s.mlb == mlb && s.clb == clb && s.boat == boat
+                && s.crb == crb && s.mrb == mrb);
+    }
+
+    void AddStates(Vector<State> v, State s) {
+        //State s = p.state;
+        if (s.mrb >= 0 && s.mlb >= 0 && s.crb >= 0 && s.clb >= 0 && (s.mlb == 0 || s.mlb >= s.clb) && (s.mrb == 0 || s.mrb >= s.crb)) {
+            v.addElement(s);
+        }
+
+    }
+
+    public Vector getChildStates() {
+        Vector<State> v = new Vector<>();
+        /* if (boat == 1) {
+            AddStates(v, new State(mlb - 1, clb - 1, 0, mrb + 1, crb + 1,boatCapacity));
+            AddStates(v, new State(mlb, clb - 2, 0, mrb, crb + 2, boatCapacity));
+            AddStates(v, new State(mlb - 2, clb, 0, mrb + 2, crb, boatCapacity));
+            AddStates(v, new State(mlb - 1, clb, 0, mrb + 1, crb, boatCapacity));
+            AddStates(v, new State(mlb, clb - 1, 0, mrb, crb + 1, boatCapacity));
+        } else {
+            AddStates(v, new State(mlb + 1, clb + 1, 1, mrb - 1, crb - 1, boatCapacity));
+            AddStates(v, new State(mlb, clb + 2, 1, mrb, crb - 2, boatCapacity));
+            AddStates(v, new State(mlb + 2, clb, 1, mrb - 2, crb, boatCapacity));
+            AddStates(v, new State(mlb + 1, clb, 1, mrb - 1, crb, boatCapacity));
+            AddStates(v, new State(mlb, clb + 1, 1, mrb, crb - 1, boatCapacity));
+        }*/
+        for (int i = 0; i < boatCapacity + 1; i++) {
+            for (int j = 0; j <= boatCapacity - i; j++) {
+                if (i != 0 || j != 0) {
+                    if (boat == 1) {
+                        AddStates(v, new State(mlb - i, clb - j, 0, mrb + i, crb + j, boatCapacity));
+                    } else {
+                        AddStates(v, new State(mlb + i, clb + j, 1, mrb - i, crb - j, boatCapacity));
+                    }
+                }
+            }
+        }
+
+        return v;
+    }
+    
+    public void printPath() {
+         if (ParentState != null)
+            ParentState.printPath();
+         System.out.println("Step " + path_cost +  " : " + toString());
+      }
+
+}
+
